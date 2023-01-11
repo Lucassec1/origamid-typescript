7cbf67ece98d87e38f47017c19ce114a651047f4
Lucassec1 <lucasemmanuelcs@gmail.com>
2023-01-11 11:26:28 -0300

    refactor: ajustando a interface de transações, deixando mais legível

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,13 @@
 import fetchData from './fetchData.js';
-
-type TransacaoPagamento = "Boleto" | "Cartão de Crédito";
-type TransacaoStatus = 
-    | "Paga" 
-    | "Aguardando pagamento" 
-    | "Recusada pela operadora de cartão" 
-    | "Estornada";
-
-interface TransacaoAPI {
-    Nome: string;
-    ID: number;
-    Data: string;
-    Status: TransacaoStatus;
-    Email: string;
-    ['Valor (R$)']: string;
-    ['Forma de Pagamento']: TransacaoPagamento;
-    ['Cliente Novo']: number;
-}
+import normalizarTransacao from './normalizarTransacao.js';
 
 async function mostrarTransacoes() {
     const data = await fetchData<TransacaoAPI[]>(
         "https://api.origamid.dev/json/transacoes.json"
     );
-    console.log(data);
-    if (data) {
-        data.forEach(item => {
-            console.log(item);
-        })
-    }
+    if (!data) return;
+    const transacoes = data.map(normalizarTransacao);
+    console.log(transacoes);
 }
 
 mostrarTransacoes();
